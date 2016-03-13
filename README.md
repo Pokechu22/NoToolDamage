227bb4ce9e818558641f91b7f7cbf50b70120bdb
Pokechu22 <Pokechu022@gmail.com>
2016-03-13 11:20:55 -0700

    Initial commit
     * config.yml and plugin.yml
     * pom.xml
     * Plugin stub
     * README and .gitignore

---
+++ README.md
@@ -0,0 +1,5 @@
+# NoToolDamage
+
+A simple spigot plugin to disable durability loss to tools in specific areas.
+
+https://redd.it/4a1tjf
---
+++ src/main/java/pokechu22/NoToolDamagePlugin.java
@@ -0,0 +1,10 @@
+package pokechu22;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Plugin that disables durability loss on tools in specific areas.
+ */
+public class NoToolDamagePlugin extends JavaPlugin {
+	
+}
